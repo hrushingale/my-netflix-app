d05a5fa9a4f513a6d8a914091243d3d464e08463
hrushingale <58002785+hrushingale@users.noreply.github.com>
2024-06-27 19:29:07 -0400

    Update accountmenu.tsx

--- components/accountmenu.tsx
+++ components/accountmenu.tsx
@@ -27,7 +27,7 @@ const AccountMenu:React.FC<AccountMenuProps> =({visible})=>{
                     </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4"/>
-                <div onClick={()=>signOut} className="px-3 text-center text-white text-sm hover:underline">
+                <div onClick={()=>signOut()} className="px-3 text-center text-white text-sm hover:underline">
                 Sign out of Netflix
                 </div>
 
